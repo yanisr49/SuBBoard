1ea14316ebc4e50b145736270bd2eac87ca2fd48
RICHARD <yanis.richard@capgemini.com>
2022-06-19 17:08:04 +0200

    Init React redux

---
+++ subboard_front/src/redux/counterAPI.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-promise-executor-return */
+// A mock function to mimic making an async request for data
+
+export function fetchCount(amount = 1) {
+    return new Promise<{ data: number }>((resolve) => setTimeout(() => resolve({ data: amount }), 500));
+}
