6b4f7ad82378fc0ad50870e1beeaafdb6b2dc2bc
yanisr49 <yanisrichard@live.fr>
2022-06-24 00:18:58 +0200

    changement du port d'écoute

--- subboard_api/index.js
+++ subboard_api/index.js
@@ -2,9 +2,9 @@ const http = require("http");
 const app = require("./src/server");
 const server = http.createServer(app);
 
-const { API_PORT } = process.env;
+const { PORT } = process.env;
 
 // server listening
-server.listen(API_PORT, () => {
-  console.log(`Server running on port ${API_PORT}`);
+server.listen(PORT || 4001, () => {
+  console.log(`Server running on port ${PORT || 4001}`);
 });
