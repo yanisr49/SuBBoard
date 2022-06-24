a9a8ccc808237c5b0f07eb69bd9f80e424627758
yanisr49 <yanisrichard@live.fr>
2022-06-24 12:49:37 +0200

    fix pour le déploiement

--- subboard_api/src/server.js
+++ subboard_api/src/server.js
@@ -26,6 +26,7 @@ const { CLIENT_ID, REDIRECT_UI, JWT_SECRET } = process.env;
 app.post('/login', async (req, res) => {
     // Vérifie que le csrf token est valide
 
+
     const cookie_g_csrf_token = req.cookies.g_csrf_token;
     const { credential, g_csrf_token } = req.body;
     /*
