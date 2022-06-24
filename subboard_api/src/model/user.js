78494a7d3857db03065ee5aa478f2997c33b8ffb
yanisr49 <yanisrichard@live.fr>
2022-06-24 20:43:04 +0200

    fix pour le déploiement

--- subboard_api/src/model/user.js
+++ subboard_api/src/model/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { SubscriptionSchema } = require('./subscription');
 
 const userSchema = new mongoose.Schema({
-    email: { type: String },
+    email: { type: String, unique: true },
     profilPicture: { type: String },
     theme: { type: String, default: 'light' },
     subscriptions: [SubscriptionSchema],
