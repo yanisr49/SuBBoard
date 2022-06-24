dda118f8bdda6e6678dc2dbbd86cf5bf88db61b7
yanisr49 <yanisrichard@live.fr>
2022-06-24 20:51:02 +0200

    fix pour le déploiement

--- subboard_api/index.js
+++ subboard_api/index.js
@@ -1,10 +1,12 @@
 const http = require("http");
 const app = require("./src/server");
-const server = http.createServer(app);
 
 const { PORT } = process.env;
 
 // server listening
-server.listen(PORT || 4001, () => {
+app.listen(PORT || 4001, () => {
   console.log(`Server running on port ${PORT || 4001}`);
 });
+
+// Export the Express API
+module.exports = app;
