f7b1e518a44b91a15c89d2537fbb557b6b3e946b
yanisr49 <yanisrichard@live.fr>
2022-12-09 12:53:16 +0100

    config headers connexion

--- subboard_api/src/server.js
+++ subboard_api/src/server.js
@@ -7,23 +7,40 @@ const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 const cors = require('cors')
 const { CLIENT_ID, FRONT_URI, JWT_SECRET } = process.env;
+const session = require("express-session")
 
 const app = express();
 
 app.use(cors(
     {
-      origin: [FRONT_URI],
+      origin: FRONT_URI,
       credentials: true,
       allowedHeaders: [
+        "Content-Type",
         "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Origin"
       ],
       exposedHeaders: [
+        "Content-Type",
         "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Origin"
       ],
     }
 ));
+
+/*
+app.use(
+    session({
+      secret: 'Super Secret (change it)',
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        sameSite: 'none', // : 'lax', // must be 'none' to enable cross-site delivery
+        secure: true,
+      }
+    })
+  );
+  */
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- subboard_front/src/graphql/graphqlClient.ts
+++ subboard_front/src/graphql/graphqlClient.ts
@@ -2,9 +2,6 @@ import { GraphQLClient } from 'graphql-request';
 import { TOKEN_STORE_NAME } from '../resources/Constants';
 
 const graphQLClient = new GraphQLClient(`${process.env.REACT_APP_API_ENDPOINT}/graphql`, {
-    headers: () => ({
-        authorization: `Bearer ${window.localStorage.getItem(TOKEN_STORE_NAME)}`,
-    }),
     credentials: 'include',
     mode: 'cors',
 });
