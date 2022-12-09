55313393576422bf54e301faa2d93d6eb4d4db3c
yanisr49 <yanisrichard@live.fr>
2022-12-09 12:54:59 +0100

    config headers connexion

--- subboard_api/src/server.js
+++ subboard_api/src/server.js
@@ -28,7 +28,6 @@ app.use(cors(
     }
 ));
 
-/*
 app.use(
     session({
       secret: 'Super Secret (change it)',
@@ -40,7 +39,6 @@ app.use(
       }
     })
   );
-  */
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
