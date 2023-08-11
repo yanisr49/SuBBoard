ecfd22ab8380c81255424fb9c24c5a8a2460f65d
yrichard <yanis.richard@capgemini.com>
2023-08-11 19:39:46 +0200

    Init java back projet

---
+++ subboard_back/src/main/java/alou/subboard_back/SubboardBackApplication.java
@@ -0,0 +1,13 @@
+package alou.subboard_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubboardBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubboardBackApplication.class, args);
+	}
+
+}
---
+++ subboard_back/src/test/java/alou/subboard_back/SubboardBackApplicationTests.java
@@ -0,0 +1,13 @@
+package alou.subboard_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubboardBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
